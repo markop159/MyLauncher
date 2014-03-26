bd650f8634b0c7528ab0258e979c1899fdfdc9bc
Marko <marko.poljansek@hotmail.com>
2014-03-26 12:31:05 +0100

    corrections

--- myLauncher/src/main/java/com/markop159/mylauncher/DrawerAdapter.java
+++ myLauncher/src/main/java/com/markop159/mylauncher/DrawerAdapter.java
@@ -1,12 +1,14 @@
 package com.markop159.mylauncher;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.BaseAdapter;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * Created by Marko on 25.3.2014.
@@ -50,9 +52,9 @@ public class DrawerAdapter extends BaseAdapter{
         LayoutInflater li= (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         
         if(convertView==null){
-            convertView=li.inflate(R.layout.drawer_item, null)
+            convertView=li.inflate(R.layout.drawer_item, null);
             
-            viewHolder=new vievHolder();
+            viewHolder=new ViewHolder();
             viewHolder.text=(TextView)convertView.findViewById(R.id.icon_text);
             viewHolder.icon=(ImageView)convertView.findViewById(R.id.icon_image);
             
