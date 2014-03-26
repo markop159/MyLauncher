f9f428460650d50daac0f5657d4e0631fe67c7cb
Marko <markop159@users.noreply.github.com>
2014-03-26 13:11:36 +0100

    Update sortApps.java

--- myLauncher/src/main/java/com/markop159/mylauncher/sortApps.java
+++ myLauncher/src/main/java/com/markop159/mylauncher/sortApps.java
@@ -1,4 +1,18 @@
 package com.markop159.mylauncher;
 
 public class sortApps{
+  public void exchange_sort(mainActivity.Pac[]pacs){
+    int i, j;
+    MainActivity.Pac temp;
+
+    for ( i = 0;  i < pacs.length - 1;  i++ ){
+      for ( j = i + 1;  j < pacs.length;  j++ ){ 
+        if ( pacs [ i ].label.compareToIgnoreCase( pacs [ j ].label ) > 0 ){   // ascending sort
+          temp = pacs [ i ];
+          pacs [ i ] = pacs [ j ];    // swapping
+          pacs [ j ] = temp;
+        }
+      }
+    } 
+  }
 }
