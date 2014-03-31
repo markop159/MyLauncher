3625ad4072d05589c42cb0d7f413177861b6fd5d
Marko <markop159@users.noreply.github.com>
2014-03-31 15:44:27 +0200

    Update AppTouchListener.java

--- myLauncher/src/main/java/com/markop159/mylauncher/AppTouchListener.java
+++ myLauncher/src/main/java/com/markop159/mylauncher/AppTouchListener.java
@@ -1,5 +1,26 @@
 package com.markop159.mylauncher;
 
 public class AppTouchListener implements OnTouchListener {
+  
+  int iconSize;
+  
+  public AppTouchListener(int size){
+    iconSize=size;
+  }
+
+  public boolean onTouch(View v, MotionEvent event){
+    
+    switch(event.getAction()){
+      case MotionEvent.ACTION_MOVE:
+        LayoutParams lp= new LayoutParams(iconSize,iconSize)
+        lp.leftMargin=(int) event.getRawX()-iconSize/2;
+        lp.topMargin=(int) event.getRawY()-iconSize/2;
+        v.setLayoutParams(lp);
+      
+    }
+    
+    return true;
+    
+  }
 
 }
