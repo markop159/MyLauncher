d3fe4cb82831ca46d184268b566493131a59fcc3
Marko <markop159@users.noreply.github.com>
2014-03-31 14:32:47 +0200

    Create DrawerLongClickListener.java

---
+++ myLauncher/src/main/java/com/markop159/mylauncher/DrawerLongClickListener.java
@@ -0,0 +1,12 @@
+package com.markop159.mylauncher;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class DrawerClickListener implements OnItemClickListener {
+
+}
